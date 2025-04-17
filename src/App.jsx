e510e20a84d6cc0ce8ba6a6a4dd5a2e2bd44b5b0
Jing Wang <jingwangw12@gmail.com>
2025-04-17 16:50:14 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  // storing post where fetch the data from the backend database
+  const [post, setPost] = useState([]);
+
+  return <>Crochet Hub</>;
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import CreatePost from "../pages/CreatePost.jsx";
+import EditPost from "../pages/EditPost.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    {/* declaring paths to the pages */}
+    <BrowserRouter>
+      <Routes>
+        {/* The homepage path */}
+        <Route path="/" element={<App />} />
+        {/* The create post page */}
+        <Route path="/create" element={<CreatePost />} />
+        {/* The edit post page where it edits a specific post based on id */}
+        <Route path="/edit/:id" element={<EditPost />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
