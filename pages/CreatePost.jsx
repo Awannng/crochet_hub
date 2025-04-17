20cb50e3dcad4a11df7d3c3da174be3a2cd7125c
Jing Wang <jingwangw12@gmail.com>
2025-04-17 18:23:30 -0400

    added create post feature that allows user to create their post

--- pages/CreatePost.jsx
+++ pages/CreatePost.jsx
@@ -1,7 +1,65 @@
 import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { supabase } from "../client";
 
 const CreatePost = () => {
-  return <>Create Post</>;
+  const navigate = useNavigate(); //a function to direct path
+
+  // storing the post information and send it to backend database
+  const [postInfo, setPostInfo] = useState({
+    title: "",
+    author: "",
+    description: "",
+  });
+
+  // change the when typing in the form
+  const handleChange = (e) => {
+    const { value, name } = e.target;
+    setPostInfo((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+
+  // creates a post in the database
+  const createPost = async (e) => {
+    e.preventDefault();
+
+    await supabase
+      .from("Post")
+      .insert({
+        title: postInfo.title,
+        author: postInfo.author,
+        description: postInfo.description,
+      })
+      .select();
+
+    navigate("/"); //after submitting the form, it goes back to the homepage
+  };
+  return (
+    <>
+      Create Post
+      <form>
+        <label htmlFor="title">Title</label> <br />
+        <input type="text" id="title" name="title" onChange={handleChange} />
+        <br />
+        <label htmlFor="author">Author</label> <br />
+        <input type="text" id="author" name="author" onChange={handleChange} />
+        <br />
+        <label htmlFor="description">Description</label> <br />
+        <textarea
+          type="text"
+          id="description"
+          name="description"
+          rows={5}
+          cols={20}
+          onChange={handleChange}
+        />
+        <br />
+        <input type="submit" value="Submit" onClick={createPost} />
+      </form>
+      <Link to="/">Back</Link>
+    </>
+  );
 };
 
 export default CreatePost;
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import "./App.css";
+import { supabase } from "../client";
 
 function App() {
   // storing post where fetch the data from the backend database
-  const [post, setPost] = useState([]);
+  const [posts, setPosts] = useState([]);
 
-  return <>Crochet Hub</>;
+  // fetch the post data from the database
+  useEffect(() => {
+    const fetchPost = async () => {
+      const { data } = await supabase
+        .from("Post")
+        .select()
+        .order("created_at", { ascending: true });
+
+      setPosts(data);
+    };
+    fetchPost();
+  }, []);
+
+  return (
+    <>
+      <Link to="/create">Create Post</Link>
+      {/* map each post on the screen */}
+      {posts &&
+        posts.map((post, index) => {
+          return (
+            <div key={index} className="post-container">
+              <p>{post.title}</p>
+              <p>{post.author}</p>
+              <p>{post.description}</p>
+            </div>
+          );
+        })}
+    </>
+  );
 }
 
 export default App;
